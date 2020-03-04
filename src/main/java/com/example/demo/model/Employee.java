f79925b94852780ec3e11ef4e3dd05d041ced19c
KimThaoUitk12 <51397805+KimThaoUitk12@users.noreply.github.com>
2020-03-04 15:37:53 +0700

    LDK-14 Description:the sam code LDK-13 - Modify annotation in EmployeeRepository.java - Add one more annotation in Employee.java

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,11 +1,14 @@
 package com.example.demo.model;
 
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@RestResource
 public class Employee {
 
     @Id
--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -3,9 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "employee", path = "employee")
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
 }
